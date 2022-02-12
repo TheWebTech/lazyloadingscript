e305c8913eb59668476efcc2f8b3d85ed619026f
Jon McLaren <TheWebTech@users.noreply.github.com>
2022-02-12 08:32:56 -0600

    update lazyload.js

--- lazyload.js
+++ lazyload.js
@@ -22,7 +22,7 @@ if ("loading" in HTMLImageElement.prototype) {
   var script = document.createElement("script");
   script.async = true;
   script.src =
-    "https://cdn.jsdelivr.net/npm/vanilla-lazyload@12.0.0/dist/lazyload.min.js";
+    "https://cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/17.3.1/lazyload.min.js";
   window.lazyLoadOptions = {
     elements_selector: "[loading=lazy]",
     //eventually more options here
